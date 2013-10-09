ea72948f2d0ead07d7ff02708d1e4053c481424f
Daniel Nelson <danielnelsonguitar@gmail.com>
2013-10-08 21:15:41 -0700

    stringifyJSON solved for arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,19 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   // your code goes here
+  if (Array.isArray(obj)) {
+  	//TODO: Test for embedded arrays
+  	var stringified = [];
+  	for (var i = 0; i <  obj.length; i++) {
+  		if (Array.isArray(obj[i])) { //if it is an embedded array
+  			stringified.push(stringifyJSON(obj[i])); //recursive function
+  		} else if (typeof(obj[i]) === "string") { //if it is already a string, add quotes
+  			stringified.push('"' + obj[i] + '"');
+  		} else { //otherwise, just convert to string
+  			stringified.push(String(obj[i]));
+  		}
+  		//join, add brackets (quotes already included b/c it is a string)
+  	}
+  	return ("[" + stringified.join(",") + "]"); 
+  } 
 };
